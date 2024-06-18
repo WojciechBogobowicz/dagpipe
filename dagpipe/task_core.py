f5f08b927042de1aba11bac3743fdb82700c08a0
Wojciech Bogobowicz <wojciech.bogobowicz@gmail.com>
2024-06-18 07:07:56 +0200

    outputs split and set name added

--- dagpipe/task_core.py
+++ dagpipe/task_core.py
@@ -7,7 +7,7 @@ Classes:
     MethodTask: Similar to task, but works with class methods.
 """
 
-from typing import Any
+from typing import Any, Iterable
 
 
 class Task:
@@ -21,14 +21,19 @@ class Task:
         args (tuple): The positional arguments to be passed to the function.
         kwargs (dict): The keyword arguments to be passed to the function.
         evaluated_result (Any): The result of the function execution.
+        name (str): The name of the task.
+        outputs_num (int): The number of outputs the function returns.
+        references (list): List of TaskReference objects.
+
     """
-    def __init__(self, func, *args, name="auto",  **kwargs):
+    def __init__(self, func, *args, name="auto", outputs_num=1, **kwargs):
         """
         Initialize a Task instance.
 
         Args:
             func (callable): The function to be executed.
             name (str, optional): Name that would be displayed in visualization.
+            outputs_num (int, optional): Number of of outputs, function returns.
             *args: Positional arguments to be passed to the function.
             **kwargs: Keyword arguments to be passed to the function.
         """
@@ -37,6 +42,9 @@ class Task:
         self.kwargs = kwargs
         self.name = name if name != "auto" else repr(self)
         self.evaluated_result = None
+        self.outputs_num=outputs_num
+        self.references = None
+        self._index = 0
 
     def run(self, *args, **kwargs) -> Any:
         """
@@ -84,6 +92,51 @@ class Task:
         args = tuple(a if not isinstance(a, Task) else a.evaluated_result for a in self.args)
         kwargs = {k: v if not isinstance(v, Task) else v.evaluated_result for k, v in self.kwargs.items()}
         return args, kwargs
+    
+    def __iter__(self):
+        """
+        Initialize an iterator for the task's outputs.
+        """
+        if self.references is None:
+            self.references = []
+            for index in range(self.outputs_num):
+                ref_name = f"{self.name}[{index}]"
+                self.references.append(TaskReference(self, index, ref_name))
+        self._index = 0
+        return self
+    
+    def __next__(self):
+        """
+        Get the next output reference in the iteration.
+
+        Returns:
+            TaskReference: The next TaskReference object.
+
+        Raises:
+            StopIteration: If there are no more outputs to iterate over.
+        """
+        if self._index == self.outputs_num:
+            raise StopIteration
+        self._index += 1
+        return self.references[self._index-1]
+
+    def set_name(self, name: str | Iterable):
+        """
+        Set the name of the task or its output references.
+
+        Args:
+            name (str or Iterable): The new name for the task or a list of names for the outputs.
+
+        Returns:
+            self: The Task instance itself.
+        """
+        if isinstance(name, str):
+            self.name = name
+        else:
+            for ref, name in zip(self, name):
+                ref.name = name
+        return self
+        
 
     def __repr__(self) -> str:
         """
@@ -94,6 +147,7 @@ class Task:
         """
         return self.func.__name__
 
+
 class MethodTask(Task):
     """
     A class representing a task that involves executing a method on an instance.
@@ -102,18 +156,20 @@ class MethodTask(Task):
         instance (Any): The instance on which the method will be executed.
     """
 
-    def __init__(self, instance, func, *args, name="auto", **kwargs):
+    def __init__(self, instance, func, *args, name="auto", outputs_num=1, **kwargs):
         """
         Initialize a MethodTask instance.
 
         Args:
             instance (Any): The instance on which the method will be executed.
             func (callable): The method to be executed.
+            name (str, optional): Name that would be displayed in visualization.
+            outputs_num (int, optional): Number of of outputs, function returns.
             *args: Positional arguments to be passed to the method.
             **kwargs: Keyword arguments to be passed to the method.
         """
         self.instance = instance
-        super().__init__(func, *args, name=name, **kwargs)
+        super().__init__(func, *args, name=name, outputs_num=outputs_num, **kwargs)
 
     def evaluate_result(self, *args, **kwargs) -> Any:
         """
@@ -132,3 +188,67 @@ class MethodTask(Task):
         if self.func.__name__ == "__call__":
             return self.instance.__class__.__name__
         return f"{self.instance.__class__.__name__}.{self.func.__name__}"
+
+
+class TaskReference(Task):
+    """
+    A class representing a reference to a specific output of a Task.
+
+    Attributes:
+        task (Task): The original task this reference points to.
+        ref_index (int): The index of the output this reference points to.
+    """
+    def __init__(self, task: Task, ref_index: int, name: str):
+        """
+        Initialize a TaskReference instance.
+
+        Args:
+            task (Task): The original task this reference points to.
+            ref_index (int): The index of the output this reference points to.
+            name (str): The name of the reference.
+        """
+        super().__init__(task.func, *task.args, name=name, outputs_num=1, **task.kwargs)
+        self.task = task
+        self.ref_index = ref_index
+        
+    def run(self, *args, **kwargs) -> Any:
+        """
+        Execute the reference task if necessary and update the evaluated result.
+
+        Args:
+            *args: Positional arguments to override currently stored arguments.
+            **kwargs: Keyword arguments to override currently stored arguments.
+
+        Returns:
+            Any: The result of the function execution.
+        """
+        if self._all_refs_ran() or (not self.task.evaluated_result):
+            self.task.run(*args, **kwargs)
+        self.evaluated_result = self.task.evaluated_result[self.ref_index]
+        return self.evaluated_result
+
+    def _all_refs_ran(self) -> bool:
+        """
+        Check if all references have run and update the reference tracking.
+
+        Returns:
+            bool: True if all references have run, False otherwise.
+        """
+        if not hasattr(self.task, "ran_refs"):
+            self.task.ran_refs = [self]
+            return True
+        if self in self.task.ran_refs:
+            self.task.ran_refs = [self]
+            return True
+        else:
+            self.task.ran_refs.append(self)
+            return False
+
+    def __repr__(self) -> str:
+        """
+        Return a string representation of the Task Reference instance.
+
+        Returns:
+            str: *task*-ref[*id*].
+        """
+        return f"{repr(self.task)}-ref[{self.ref_index}]"
--- dagpipe/task_decorators.py
+++ dagpipe/task_decorators.py
@@ -14,7 +14,7 @@ import inspect
 from dagpipe.task_core import Task, MethodTask
 
 
-def task(name="auto") -> callable:
+def task(name="auto", outputs_num: int = 1) -> callable:
     """
     A decorator that wraps a function in a Task instance.
     Task postpone function execution, 
@@ -22,31 +22,32 @@ def task(name="auto") -> callable:
 
     Args:
         name (str, optional): Name used in visualization.
+        outputs_num (int, optional): Number of of outputs, function returns. 
 
     Returns:
         callable: A wrapped function that returns a Task instance.
     """
     def decorator(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            return Task(func, *args, name=name, **kwargs)
+        # @functools.wraps(func)
+        def wrapper(*args, **kwargs) -> Task:
+            return Task(func, *args, name=name, outputs_num=outputs_num, **kwargs)
         return wrapper
     return decorator
 
 
-def method_task(name="auto") -> callable:
+def method_task(name="auto", outputs_num=1) -> callable:
     """
     Similar to 'task' but works with class method instead of standalone functions. 
 
     Args:
         name (str, optional): Name used in visualization.
-
+        outputs_num (int, optional): Number of of outputs, function returns.
     Returns:
         callable: A wrapped method that returns a MethodTask instance.
     """
     def decorator(method):
-        @functools.wraps(method)
-        def wrapper(instance, *args, **kwargs):
-            return MethodTask(instance, method, *args, name=name, **kwargs)
+        # @functools.wraps(method)
+        def wrapper(instance, *args, **kwargs) -> Task:
+            return MethodTask(instance, method, *args, name=name, outputs_num=outputs_num, **kwargs)
         return wrapper
     return decorator
